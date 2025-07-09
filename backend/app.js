668e5df9e2640d6d54fc4db6e3e957cd272162e5
Carlos Rodríguez <145693019+44devCarlos@users.noreply.github.com>
2025-07-08 21:44:45 -0500

    Inicio del backend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 import cors from "cors";
+import express from "express";
+import { PORT } from "./config.js";
 
-export const createApp = (express) => {
+export const createApp = (modelos) => {
 	const app = express();
 	app.use(cors());
+
+	app.use((req, res) => {
+		res.status(404).json({ error: "Recurso no encontrado" });
+	});
 
 	app.listen(PORT, () => {
 		console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import { createApp } from './app.js';
+// File: backend/app.js
+
+const modelos = {
+  
+}
+
+createApp({ modelos })
